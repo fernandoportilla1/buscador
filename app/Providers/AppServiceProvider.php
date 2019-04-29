dfb47adde2b35f0f91f95e2dfdcbd3fb999ee860
Fernando <fernando93f@gmail.com>
2019-04-29 14:15:51 -0500

    Added elasticserach client to service container

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Elasticsearch\ClientBuilder;
+use Elasticsearch\Client;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(Client::class, function($app) {
+            return ClientBuilder::create()
+                ->setHosts([env('SEARCH_HOST')])
+                ->build();
+        });
     }
 
     /**
